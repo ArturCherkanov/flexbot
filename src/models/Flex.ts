687cabfd51f402c10c0a0f81afb9b0ad0b539298
Artur <artur.cherkanov>
2019-11-01 12:50:24 +0300

    extend Flex model

--- src/models/Flex.ts
+++ src/models/Flex.ts
@@ -1,7 +1,15 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const FlexSchema = new Schema({
-    name: { type: String, default: 'No name' },
+    name: { 
+        type: String, default: 'No name'
+     },
+     location: { 
+        type: String, default: 'no location'
+     },
+     data: {
+        type: String, default: 'No data'
+     }
 });
 
 module.exports = mongoose.model("Flex", FlexSchema);
