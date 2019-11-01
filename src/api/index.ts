2ced326ee369882ee46064109a6b4e0337628529
Artur <artur.cherkanov>
2019-11-01 12:51:00 +0300

    add LUIS api

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,17 @@ import TelegramBot from "node-telegram-bot-api";
 const translate = require('yandex-translate')('trnsl.1.1.20191028T211302Z.cb09357ddb661c0b.c749257fcc90f0dc715ff27d55d1d3e034125197');
 import express from 'express';
 const app = express()
+import axios from 'axios';
+import { inflateSync } from "zlib";
 
+const axiosLusiInstance = axios.create({
+
+    timeout: 15000,
+    headers: {
+        "Content-Type": "application/json",
+        "Ocp-Apim-Subscription-Key": '0945e8cd992e4fb9a276279043715f74',
+    }
+});
 const Flex = require('../models/Flex');
 
 type CommandHandler = (msg: any, match: Array<string>) => void;
@@ -16,15 +26,33 @@ export const createCommands = (bot: TelegramBot) => ({
         const chatId: string = msg.chat.id;
         const resp: string = match[1];
         const flex = new Flex();
-        let translatedText: string  = '';
+        let translatedText: string = '';
         // flex.name = resp;
 
-        translate.translate(resp, { to: 'en' }, function(err, res) {
+        translate.translate(resp, { to: 'en' }, function (err, res) {
             console.log(typeof res.text[0]);
-            translatedText = res.text[0]
-          });
+            translatedText = res.text[0];
+            axiosLusiInstance.get('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/3e9994e5-907a-42b6-8bf6-4600ded14589?staging=true&verbose=true&timezoneOffset=180&subscription-key=0945e8cd992e4fb9a276279043715f74', {
+                params: {
+                    q: translatedText
+                }
+            }).then(res => {
 
-          //don't input 
+                let village = res.data.entities.map(item => {
+
+                    let flexParsedModel = new Object();
+                    
+                    flexParsedModel[item.role] = item.entity;
+                    return flexParsedModel
+                })
+                console.log(village)
+            })
+                .catch((err => {
+                    console.log(err)
+                }))
+        });
+
+        //don't input 
         // flex.save()
         //     .then(() => {
         //         console.log('success')
